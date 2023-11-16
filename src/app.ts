f3506e2beb8f2f250e9bb341b5cdbd0cf84213bf
mary-ue <maryandreeva100@gmail.com>
2023-11-16 19:29:49 +0300

    async functions

--- src/app.ts
+++ src/app.ts
@@ -120,7 +120,7 @@ interface User {
   }
 
   // Async logic
-  async function getAllTodos() {
+  async function getAllTodos(): Promise<Todo[]> {
     try {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/todos?_limit=15'
@@ -132,10 +132,12 @@ interface User {
       if (error instanceof Error) {
         alertError(error);
       }
+
+      return [];
     }
   }
 
-  async function getAllUsers() {
+  async function getAllUsers(): Promise<User[]> {
     try {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/users?_limit=5'
@@ -147,6 +149,8 @@ interface User {
       if (error instanceof Error) {
         alertError(error);
       }
+
+      return [];
     }
   }
 
